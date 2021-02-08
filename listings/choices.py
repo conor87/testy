5805c58bf57bc871eee2923f23fc3b930261f7c7
conor1987 <karoldabko@gmail.com>
2021-02-08 13:03:03 +0100

    first_commit

---
+++ listings/choices.py
@@ -0,0 +1,15 @@
+bedroom_choices = {
+    '1':1,
+    '2':2,
+    '3':3,
+}
+
+price_choices = {
+    '100000':'$100,000',
+    '200000':'$200.000'
+}
+
+state_choices = {
+    'AK': 'Alaska',
+    'AL': 'Alabama'
+}
---
+++ listings/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from listings.choices import price_choices, bedroom_choices, state_choices
+
+from .models import Listing
+
+# Create your views here.
+
+def index(request):
+
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {
+        'listings': paged_listings
+    }
+
+    return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
+    listing=get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing': listing
+
+    }
+
+
+    return render(request, 'listings/listing.html', context)
+
+def search(request):
+
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'listings': queryset_list,
+    }
+    return render(request, 'listings/search.html', context)
